fed2b0a6d71e9739db000a575e57bf2db1a75fd0
Orion-oro-12 <sergiominanor@gmail.com>
2022-01-15 18:24:02 +0100

    Changed compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,4 @@
 clang++ -c src/funcion.cpp -Iinclude
-clang++ -c mainTime.cpp -Iinclude
 clang++ -c main.cpp -Iinclude
 clang++ -o ejecutable main.o funcion.o
-clang++ -o ejecutableTime mainTime.o funcion.o
 rm *.o
